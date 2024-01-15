b643c1643ffca83530d542c4bb69501c137eec43
Marian Sacaliuc <msacaliu@c1s9d44.42wolfsburg.de>
2024-01-15 17:12:51 +0100

    works perfectly

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msacaliu <msacaliu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 13:15:38 by msacaliu          #+#    #+#             */
+/*   Updated: 2024/01/15 13:39:55 by msacaliu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*read_function(int fd, char *str);
+char	*ft_getline(char *fullStr);
+char	*ft_getrest(char *fullStr);
+
+#endif
